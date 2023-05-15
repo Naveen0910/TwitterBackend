0af687a5b924cbf2c6cb045dbe1df2c837cd5028
Naveen0910 <naveen850850@gmail.com>
2023-05-15 16:41:24 +0530

    ServerInitializationAndDBConnnection

---
+++ index.js
@@ -0,0 +1,27 @@
+/* CODE FOR INTIALIZING CONNECTION WITH DB AND STARTING SERVER */
+
+const express = require('express')
+const  path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+
+const app = express();
+const dbPath = path.join(__dirname,'twitterClone.db')
+
+let db = null;
+
+const intializeDBandServer=async()=>{
+    try {
+        db = await open({
+            filename:dbPath,
+            driver:sqlite3.Database,
+        })
+        app.listen(5000, ()=>{console.log('intialization successfull check port 5000')})
+    } catch (e) {
+        console.log(`DB Error: ${e.message}`);
+        process.exit(1);
+    }
+}
+
+intializeDBandServer();
